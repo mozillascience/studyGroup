5e6c590a31e2e14c4438743b660feea43de4bcb2
BillMills <mills.wj@gmail.com>
2015-08-11 21:56:22 -0700

    pep8 (mostly)

--- scripts/updateCalendar.py
+++ scripts/updateCalendar.py
@@ -1,22 +1,24 @@
-##################################################################################################
+#######################################################################
 # date: 2015-07-28
-# author: Thea Van Rossum theavanrossum@gmail.com 
+# author: Thea Van Rossum theavanrossum@gmail.com
 # functionality:
 #    1. Creates a Google Calendar API service object
-#    2. Deletes all events in the calendar in case changes have been made to existing events
-#    3. Create events based on all the posts in "_posts" (POSTS_DIRECTORY)
+#    2. Deletes all events in the calendar in case changes have been
+#       made to existing events
+#    3. Create events based on all the posts in
+#       "_posts" (POSTS_DIRECTORY)
 #    Commented out: 4. Print next 10 events
 #
 #    Will not add an event if it is missing one of the REQUIRED_FIELDS
 #
-# To modify and use: 
+# To modify and use:
 #   1. See google docs to get setup with credentials:
 #      https://developers.google.com/google-apps/calendar/quickstart/python
-#   2. Update the variables indicated below (APPLICATION_NAME, CALENDAR_ID, POSTS_DIRECTORY, TIME_ZONE_SRT
-#   3. run from scripts/ using: python updateCalendar.py --noauth_local_webserver
-#
-#
-##################################################################################################
+#   2. Update the variables indicated below (APPLICATION_NAME,
+#      CALENDAR_ID, POSTS_DIRECTORY, TIME_ZONE_SRT
+#   3. run from scripts/ using:
+#      python updateCalendar.py --noauth_local_webserver
+########################################################################
 
 
 import httplib2
@@ -37,24 +39,30 @@ try:
 except ImportError:
     flags = None
 
-# Modify these variables in step 2 above ------------------- 
-APPLICATION_NAME = 'test' # app name you created in step one above
-CALENDAR_ID='mills.wj@gmail.com' # google account name you created for your calendar
-TIME_ZONE_STR = 'America/Vancouver' # not sure? check here: https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
-#-----------------------------------------------------------
+# Modify these variables in step 2 above -------------------
+# APPLICATION_NAME: app name you created in step one above:
+APPLICATION_NAME = 'test'
+# CALENDAR_ID: google account name you created for your calendar:
+CALENDAR_ID = 'mills.wj@gmail.com'
+# TIME_ZONE_STR: check here:
+# https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+TIME_ZONE_STR = 'America/Vancouver'
+# -----------------------------------------------------------
 
 SCOPES = 'https://www.googleapis.com/auth/calendar'
 CLIENT_SECRET_FILE = 'client_secret.json'
-DEFAULT_START_TIME = "15:30" # will be overridden by startTime in _posts
-DEFAULT_END_TIME = "16:30" # will be overridden by endTime in _posts
-REQUIRED_FIELDS = [ 'title', 'location', 'text', 'link', 'date' ]
-POSTS_DIRECTORY="../_posts"
+DEFAULT_START_TIME = "15:30"  # will be overridden by startTime in _posts
+DEFAULT_END_TIME = "16:30"  # will be overridden by endTime in _posts
+REQUIRED_FIELDS = ['title', 'location', 'text', 'link', 'date']
+POSTS_DIRECTORY = "../_posts"
+
 
 def main():
     """
-    1. Creates a Google Calendar API service object 
-    2. Deletes all events in the calendar in case changes have been made to existing events
-    3. Create events based on all the posts in "_posts" (POSTS_DIRECTORY) 
+    1. Creates a Google Calendar API service object
+    2. Deletes all events in the calendar in case
+       changes have been made to existing events
+    3. Create events based on all the posts in "_posts" (POSTS_DIRECTORY)
     Commented out: 4. Print next 10 events
 
     Will not add an event if it is missing one of the REQUIRED_FIELDS
@@ -63,10 +71,10 @@ def main():
     http = credentials.authorize(httplib2.Http())
     service = discovery.build('calendar', 'v3', http=http)
 
-    #clear the calendar
+    # clear the calendar
     service.calendars().clear(calendarId=CALENDAR_ID).execute()
 
-    #create events
+    # create events
     for inputPath in glob.glob(os.path.join(POSTS_DIRECTORY, '*.markdown')):
         eventDict = parseEventPost(inputPath)
         events = getAllEvents(service)
@@ -77,12 +85,9 @@ def main():
             event = service.events().insert(calendarId=CALENDAR_ID, body=event).execute()
             print 'Event created: %s' % (event.get('summary'))
 
-    # print next 10
-#    printNextEvents(service, 10)
-
 
 def printNextEvents(service, numEvents):
-    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+    now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
     print 'Getting the upcoming %d events' % numEvents
     eventsResult = service.events().list(
         calendarId=CALENDAR_ID, timeMin=now, maxResults=numEvents, singleEvents=True,
@@ -95,11 +100,13 @@ def printNextEvents(service, numEvents):
         start = event['start'].get('dateTime', event['start'].get('date'))
         print start, event['summary']
 
+
 def getAllEvents(service):
     eventsResult = service.events().list(
         calendarId=CALENDAR_ID, singleEvents=True, orderBy='startTime').execute()
     events = eventsResult.get('items', [])
     return events
+
 
 def parseEventPost(inputPath):
     eventDict = {}
@@ -107,27 +114,30 @@ def parseEventPost(inputPath):
     eventDict['endTime'] = DEFAULT_END_TIME
     f = open(inputPath, 'r')
     for line in f:
-        listedline = line.strip().split(':',1) # split around the : sign
-        if len(listedline) > 1: # we have the = sign in there
+        listedline = line.strip().split(':', 1)  # split around the : sign
+        if len(listedline) > 1:  # we have the = sign in there
             eventDict[listedline[0].strip()] = listedline[1].strip()
     return eventDict
+
 
 def isEventComplete(eventDict, sourcePath):
     isComplete = 1
     for field in REQUIRED_FIELDS:
-        if not field in eventDict:
+        if field not in eventDict:
             print "Error: event missing %s (%s)" % field, sourcePath
             isComplete -= 1
     return isComplete
 
+
 def makeDateTime(dateStr, hourMinStr):
-    #date like "2014-07-25"
-    #hourMinStr like "15:30"
+    # date like "2014-07-25"
+    # hourMinStr like "15:30"
     timeStr = hourMinStr[1:-1]
     date = dateStr.split('-')
     TIME_ZONE_HR = ':00'+pytz.timezone(TIME_ZONE_STR).localize(datetime.datetime(int(date[0]), int(date[1]), int(date[2]))).strftime('%z')
     TIME_ZONE_HR = TIME_ZONE_HR[:-2] + ':' + TIME_ZONE_HR[-2:]
-    return dateStr +"T" + timeStr + TIME_ZONE_HR
+    return dateStr + "T" + timeStr + TIME_ZONE_HR
+
 
 def createEvent(eventDict):
     event = {
@@ -136,16 +146,16 @@ def createEvent(eventDict):
         'description':  eventDict['text']+"\n"+eventDict['link'],
         'start': {
             'dateTime': makeDateTime(eventDict['date'], eventDict['startTime']),
-            'timeZone': TIME_ZONE_STR ,
+            'timeZone': TIME_ZONE_STR
             },
         'end': {
             'dateTime':  makeDateTime(eventDict['date'], eventDict['endTime']),
-            'timeZone': TIME_ZONE_STR ,
+            'timeZone': TIME_ZONE_STR
             },
         'reminders': {
             'useDefault': False,
             'overrides': [
-                {'method': 'email', 'minutes': 60 * 24 * 2}, # 2 days
+                {'method': 'email', 'minutes': 60 * 24 * 2},  # 2 days
                 ],
             },
         }
@@ -175,7 +185,7 @@ def get_credentials():
         flow.user_agent = APPLICATION_NAME
         if flags:
             credentials = tools.run_flow(flow, store, flags)
-        else: # Needed only for compatability with Python 2.6
+        else:  # Needed only for compatability with Python 2.6
             credentials = tools.run(flow, store)
         print 'Storing credentials to ' + credential_path
     return credentials
@@ -183,4 +193,3 @@ def get_credentials():
 
 if __name__ == '__main__':
     main()
-
