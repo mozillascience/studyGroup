482f58acda48398124334fb8209eb00348040d84
Lina Tran <lina.mntran@gmail.com>
2017-07-26 11:46:29 -0400

    change map to list comp

--- lessons/python/cpu-multiprocessing/joblib_tutorial_2_multiple_args.py
+++ lessons/python/cpu-multiprocessing/joblib_tutorial_2_multiple_args.py
@@ -9,12 +9,12 @@ if __name__ == '__main__':
     inputs = list(range(10))
     print('Input   :', inputs)
 
-    builtin_outputs = list(map(f, inputs))
+    builtin_outputs = [f(x,y) for x in inputs]
     print('Built-in:', builtin_outputs)
 
     #number of threads to use
     pool_size = cpu_count()
     print("Number of processes:", pool_size)
-    outputs = Parallel(n_jobs=pool_size)(delayed(f)(x,y) for x,y in inputs)
+    outputs = Parallel(n_jobs=pool_size)(delayed(f)(x,y) for x in inputs)
 
-    print('Pool:', pool_outputs)
+    print('Pool:', outputs)
